891fd0bbcaabd9d190b9eb8612b0a15d3d3724b2
Minyoung Woo <minyoungwoo19990718@gmail.com>
2026-05-05 17:11:07 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 # Set up the page
-st.set_page_config(page_title="Glory ManU", page_icon="🦜")
+st.set_page_config(page_title="Glory ManU", page_icon="⚽")
 
 st.write("VMS Group, Eric Wang")
