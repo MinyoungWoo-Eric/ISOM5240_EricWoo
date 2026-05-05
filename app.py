b4a095da426ae7d66acdfbdae5554ecfd2bbe16d
Minyoung Woo <minyoungwoo19990718@gmail.com>
2026-05-05 17:23:22 +0800

    Add header for VMS Soccer Team

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import streamlit as st
 # Set up the page
 st.set_page_config(page_title="Glory ManU", page_icon="⚽")
 
+st.header("VMS Soccer Team ⚽")
+
 st.write("VMS Group, Eric Wang")
 st.write("VMS Group, Eric Woo")
 st.write("VMS Group, Antonio Hernandez Cerdo")
