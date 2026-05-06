24cde95b10534bf801e58ee2e1bba58365fcaa11
Minyoung Woo <minyoungwoo19990718@gmail.com>
2026-05-06 19:45:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ st.set_page_config(page_title="Glory ManU", page_icon="⚽")
 
 st.header("VMS Soccer Team ⚽")
 
-st.write("Hello, *World!* :sunglasses:")
+st.subheader("Hello, *World!* :sunglasses:")
 st.write("Eric Wang")
 st.write("Eric Woo")
 st.write("Antonio Hernandez Cerdo")
+
+st.badge("Yeah")
